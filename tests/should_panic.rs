39db3adaac9be9376754449b78b1c51cd4411b08
Ryan Sullivan <rysulliv@redhat.com>
2024-11-06 13:33:49 -0500

    os-practice: remove should_panic from testing harness
    Allows us to now run the should_panic test directly from _start in the
    module and means we don't need to do all of the harness setup from
    before. Down the line this could be used to specify tests with side
    effects be run in a specific order rather than in the order the test
    harnes chooses
    
    Signed-off-by: Ryan Sullivan <rysulliv@redhat.com>

--- tests/should_panic.rs
+++ tests/should_panic.rs
@@ -1,10 +1,5 @@
 #![no_std]
 #![no_main]
-#![feature(custom_test_frameworks)]
-// defining custom test runner for tests that should panic since #[should_panic] is
-// unavailable without the std lib
-#![test_runner(test_runner)]
-#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 use os_practice::{exit_qemu, serial_println};
@@ -18,21 +13,12 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    test_main();
+    should_fail();
+    serial_println!("[test did not panic]");
+    exit_qemu(os_practice::QEMUExitCode::Failure);
     loop {}
 }
 
-pub fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-        serial_println!("[test did not panic]");
-        exit_qemu(os_practice::QEMUExitCode::Failure);
-    }
-    exit_qemu(os_practice::QEMUExitCode::Success);
-}
-
-#[test_case]
 fn should_fail() {
     use os_practice::serial_print;
     serial_print!("should_panic::should_fail...\t");
