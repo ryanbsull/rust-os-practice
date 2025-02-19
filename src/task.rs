d282739fb6e65ba794778c5c38265c708b45aa47
Ryan B. Sullivan <ryanbakersullivan@gmail.com>
2025-02-18 21:53:40 -0500

    os_practice: start adding in new, better executor to capitalize on async functionality
    Signed-off-by: Ryan B. Sullivan <ryanbakersullivan@gmail.com>

--- src/task.rs
+++ src/task.rs
@@ -1,18 +1,35 @@
 use alloc::boxed::Box;
 use core::task::{Context, Poll};
-use core::{future::Future, pin::Pin};
+use core::{
+    future::Future,
+    pin::Pin,
+    sync::atomic::{AtomicU64, Ordering},
+};
 
+pub mod exec;
 pub mod keyboard;
 pub mod simple_exec;
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+struct TaskId(u64);
+
+impl TaskId {
+    fn new() -> Self {
+        static NEXT_ID: AtomicU64 = AtomicU64::new(0);
+        TaskId(NEXT_ID.fetch_add(1, Ordering::Relaxed))
+    }
+}
+
 // type wrapper for a pinned, heap allocated, dynamically sized Future value for some Task t
 pub struct Task {
+    id: TaskId,
     future: Pin<Box<dyn Future<Output = ()>>>,
 }
 
 impl Task {
     pub fn new(future: impl Future<Output = ()> + 'static) -> Self {
         Task {
+            id: TaskId::new(),
             future: Box::pin(future),
         }
     }
